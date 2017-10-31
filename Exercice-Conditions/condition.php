fdd5db93c681fc7c4930726e5db2b8e893d494b8
Unknown <thomaspoint@gmail.com>
2017-10-31 09:48:42 +0100

    exo suite

--- Exercice-Conditions/condition.php
+++ Exercice-Conditions/condition.php
@@ -20,38 +20,65 @@
 
   <?php
   $langueYes = $_POST['Yes'];
-  $langueNo = $_POST['No'];
-  $homme = $_POST['Homme'];
+  // $homme = $_POST['Homme'];
   $femme = $_POST ['Femme'];
   $age = $_POST['Age'];
 
-    if ($age < 12){
+    if ($age < 12 AND $femme == true){
+      echo 'Salut Petite';
+    } elseif ($age < 12 AND $femme == false) {
       echo 'Salut Petit';
-    } elseif ($age >= 12 AND  $age < 18 AND $homme == true) {
+    } elseif ($age >= 12 AND  $age < 18 AND $femme == false) {
       echo "Salut l'adolescent";
-    } elseif ($age > 18 AND $age < 115) {
-      echo "Salut l'adulte";
     } elseif ($age >= 12 AND $age < 18 AND $femme == true) {
       echo "Salut l'adolescente";
+    } elseif ($age > 18 AND $age < 115 AND $femme == false) {
+      echo "Salut Monsieur";
+    } elseif ($age > 18 AND $age < 115 AND $femme == true) {
+      echo "Bonjour Madame";
     } else {
       echo "Wow toujours vivant?";
     }
 
     if ($age < 12 AND $langueYes == true AND $homme == true) {
-      echo "Hello Boy";
+      echo " Hello Boy";
     } elseif ($age < 12 AND $langueYes == true AND $femme == true) {
-      echo "Hello Girl";
-    } elseif ($langueNo == true) {
-      echo " Il serait temps d'apprendre l'anglais!";
+      echo " Hello Girl";
+    } elseif ($age >= 12 AND  $age < 18 AND $langueYes == true AND $femme == true) {
+      echo " Hello Teenage Girl";
+    } elseif ($age >= 12 AND  $age < 18 AND $langueYes == true AND $femme == false) {
+      echo " Hello Teenage Boy";
+    } elseif ($age > 18 AND $age < 115 AND $langueYes == true AND $femme == true) {
+      echo " Hello Lady";
+    } elseif ($age > 18 AND $age < 115 AND $langueYes == true AND $femme == false) {
+      echo " Hello Mister";
+    } elseif ($age >= 115 AND $langueYes == true AND $femme == true) {
+      echo " Wow still alive, old lady?";
+    } elseif ($age >= 115 AND $langueYes == true AND $femme == false) {
+      echo " Wow still alive, old man?";
     } else {
-      echo 'Hello';
+      echo " Il serait temps d'apprendre l'anglais!";
     }
-
-
-   ?>
+  ?>
  </br>
   <?php
-
+    $note = 3;
+    if($note <= 3){
+      echo "Ce travail est nul.";
+    } elseif ($note > 3 AND $ $note <= 9) {
+      echo "Ce travail n'est pas terrible.";
+    } elseif ($note = 10 ) {
+      echo "Tout juste !";
+    } elseif ($note > 10 AND $ $note <= 14) {
+      echo "C'est pas mal!";
+    } elseif ($note > 14 AND $ $note <= 18) {
+      echo "Bravo";
+    } else {
+      echo "Police! Arrêtez ce tricheur!";
+    }
+  ?>
+</br>
+  <?php
     $chambre_en_ordre = 'en ordre';
     if ($chambre_en_ordre == 'immaculé') {
       echo 'Range ta chambre espèce de Bonobo!';
---
+++ Exercice-Conditions/conditions-switch.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $note = 3;
+  switch ($note) {
+    case '0':
+      echo 'la valeur est égal a 0';
+      break;
+    case '1':
+      echo 'la valeur est égal a 1';
+      break;
+    case '2':
+      echo 'la valeur est égal a 2';
+      break;
+    case '3':
+      echo 'la valeur est égal a 3';
+      break;
+    case '4':
+      echo 'la valeur est égal a 4';
+      break;
+    case '5':
+      echo 'la valeur est égal a 5';
+      break;
+    case '6':
+      echo 'la valeur est égal a 6';
+      break;
+    case '7':
+      echo 'la valeur est égal a 7';
+      break;
+    case '8':
+      echo 'la valeur est égal a 8';
+      break;
+    case '9':
+      echo 'la valeur est égal a 9';
+      break;
+    case '10':
+      echo 'la valeur est égal a 10';
+      break;
+    default:
+      echo 'la valeur est égal a plus de 10';
+      break;
+  }
+  ?>
+  <form method='post' action="conditions-switch.php">
+    Age : <input type="number" name='Age'> </br>
+    Homme : <input type='radio' name='Homme'>
+    Femme : <input type="radio" name='Femme'> </br>
+    <input type="submit" value='OK'>
+  </form>
+  <?php
+
+  $age = $_POST['Age'];
+  $sexe = $_POST['Femme'];
+
+  if($age => 21 AND $age =< 40 AND $sexe = true){
+    echo "Bonjour, bienvenue parmi nous!";
+  } else {
+    echo "Désolé, vous ne remplissez pas les critères de sélection.";
+  }
+
+  ?>
+</body>
+</html>
