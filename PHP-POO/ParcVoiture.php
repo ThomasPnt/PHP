61af35c97df57fd4dbb50662726e5e770fcedef3
Unknown <thomaspoint@gmail.com>
2017-11-16 14:29:11 +0100

    mom

--- PHP-POO/ParcVoiture.php
+++ PHP-POO/ParcVoiture.php
@@ -9,8 +9,69 @@ Class Voiture{
   private $marque;
   public $couleur;
   public $poid;
+  private $pays;
 
-
+  public function __construct($model,$poid,$pays,$km,$circulation){
+    $this->model($model);
+    $this->poid($poid);
+    $this->from($pays);
+    $this->km($km);
+    $this->age($circulation);
+  }
+  private function model($model){
+    if($model === 'Audi'){
+      echo "RESERVED";
+    } else {
+      echo "FREE";
+    }
+  }
+  private function poid($poid){
+    if($poid > 3.5){
+      echo "UTILITAIRE";
+    }  else {
+      echo "COMMERCIAL";
+    }
+  }
+  private function from($pays){
+    switch ($pays) {
+      case 'BE':
+        echo "FROM BELGIUM";
+        break;
+      case 'FR':
+        echo "FROM FRANCE";
+        break;
+      case 'GR':
+        echo "FROM GERMANY";
+        break;
+      default:
+        echo "FROM NOWHERE";
+        break;
+    }
+  }
+  private function km($km){
+    if($km < 100.000){
+      echo "LOW";
+    } elseif ($km >=100.000 AND $km < 200.000) {
+      echo "MIDDLE";
+    } else {
+      echo "HIGH";
+    }
+  }
+  private function age($circulation){
+    $temps = date('Y');
+    $age = $temps - $circulation;
+    echo $age . "ANS";
+  }
+  public function drive(){
+    echo $this->km += 100.000;
+  }
 }
+
+$Audi = new Voiture('Audi', 3.7,'BE',50.000, 2011); echo '</br>';
+$Dacia = new Voiture('Dacia',5.4,'FR',12.000,2016); echo '</br>';
+
+echo $Audi->drive(50.000);
+
+
 
  ?>
