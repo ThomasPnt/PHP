4d553aa1d575c00f4146e535673a2e75138506ed
Unknown <thomaspoint@gmail.com>
2017-11-17 12:30:59 +0100

    dada

--- PHP-POO/ParcVoiture.php
+++ PHP-POO/ParcVoiture.php
@@ -79,23 +79,25 @@ Class Voiture{
   }
 }
 
-$Audi = new Voiture('Audi', 3.7,'BE',50000, 2011); echo '<hr>';
-$Dacia = new Voiture('Dacia',5.4,'FR',12000,2016); echo '<hr>';
-$Mazda = new Voiture('Mazda',0.9,'JA',110000,2009); echo '<hr>';
+$Audi = new Voiture('Audi', 3.7,'BE',50000, 2011);
+$Dacia = new Voiture('Dacia',5.4,'FR',12000,2016);
+$Mazda = new Voiture('Mazda',0.9,'JA',110000,2009);
+$Nissan = new Voiture('Nissan',1.5,'GR',90000,2010);
+$BMW = new Voiture('BMW',1.8,'IT',2500,2017);
+$Audi_100 = new Voiture('Audi',3.6,'GR',300000,1996);
 
-$voitures = [$Audi,$Dacia,$Mazda];
+$voitures = [$Audi,$Dacia,$Mazda,$Nissan,$BMW,$Audi_100];
 foreach ($voitures as $voiture) {?>
-  <table>
-    <tr>
-      <td><?php echo $voiture->getModel() ?></td>
-      <td><?php echo $voiture->getPoid() ?></td>
-      <td><?php echo $voiture->getPays() ?></td>
-      <td><?php echo $voiture->getKm() ?></td>
-      <td><?php echo $voiture->getCirculation() ?></td>
-    </tr>
+  <table border='1'  width="100%">
+      <tr align="center"><td><?php echo $voiture->getModel() ?></td></tr>
+      <tr><td><?php echo $voiture->getPoid() ?></td></tr>
+      <tr><td><?php echo $voiture->getPays() ?></td></tr>
+      <tr><td><?php echo $voiture->getKm() ?></td></tr>
+      <tr><td><?php echo $voiture->getCirculation() ?></td></tr>
   </table>
 <?php }
 
+echo '</br>';echo '</br>';echo '</br>';
 echo $Audi->drive(); echo '<hr>';
 echo $Audi->getKm();
 
